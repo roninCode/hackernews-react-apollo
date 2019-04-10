37ecad611aba69968b0208755b0dbd193a0d4f07
Jamie MacKillop <jamie@ritani.com>
2019-04-10 12:03:12 -0400

    Add CreateLink component. Varified that it is post new links

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 import logo from '../logo.svg';
 import '../App.css';
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <CreateLink />
     );
   }
 }
--- src/components/LinkList/index.jsx
+++ src/components/LinkList/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Link from '../Link';
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 
 class LinkList extends Component {
